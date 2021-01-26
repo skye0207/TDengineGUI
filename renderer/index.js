c00e8554079045523da671dbd79ac4a9843d5c1f
skye <wangguan.i@unicloud.com>
2021-01-26 11:57:14 +0800

    demo

--- renderer/index.js
+++ renderer/index.js
@@ -4,6 +4,7 @@ new Vue({
     el: '#app',
     data: function() {
       return { 
+        drawer: false,
         dialogVisible: false,
         linkForm: {
           host:"",
@@ -11,42 +12,7 @@ new Vue({
           user:"",
           password:"",
         },
-        formLabelWidth: '80px',
-        DBs: [{
-            label: '一级 1',
-            children: [{
-              label: '二级 1-1',
-              children: [{
-                label: '三级 1-1-1'
-              }]
-            }]
-          }, {
-            label: '一级 2',
-            children: [{
-              label: '二级 2-1',
-              children: [{
-                label: '三级 2-1-1'
-              }]
-            }, {
-              label: '二级 2-2',
-              children: [{
-                label: '三级 2-2-1'
-              }]
-            }]
-          }, {
-            label: '一级 3',
-            children: [{
-              label: '二级 3-1',
-              children: [{
-                label: '三级 3-1-1'
-              }]
-            }, {
-              label: '二级 3-2',
-              children: [{
-                label: '三级 3-2-1'
-              }]
-            }]
-          }],
+        formLabelWidth: '80px'
       }
     },
     methods: {
