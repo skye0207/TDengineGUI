bacd8c19a2e3737933f4d165cfa4270819b1c677
skye <wangguan.i@unicloud.com>
2021-01-26 22:29:27 +0800

    add some 样式demo

--- renderer/index.js
+++ renderer/index.js
@@ -12,7 +12,24 @@ new Vue({
           user:"",
           password:"",
         },
-        formLabelWidth: '80px'
+        formLabelWidth: '80px',
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
       }
     },
     methods: {
