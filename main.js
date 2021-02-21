cd85b55229837a4d4a06b62d769a0b6a6bfb0008
skye <wangguan.i@unicloud.com>
2021-02-21 22:58:02 +0800

    修改表格最大高度

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 1000,
-    height: 800,
+    width: 1200,
+    height: 880,
     webPreferences: {
       nodeIntegration: true
     }
