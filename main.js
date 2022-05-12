13fc7d163d0b21c400f096ce3a63420580b76f6b
胡益 <326170945@qq.com>
2022-05-12 13:40:38 +0800

    去掉菜单栏

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
   const win = new BrowserWindow({
+    autoHideMenuBar: true,
     width: 1200,
-    height: 880,
+    height: 900,
+    // show: false, //关闭窗口显示
     webPreferences: {
       nodeIntegration: true
     }
   })
 
   win.loadFile('./renderer/index.html')
+  // win.maximize() //设置最大化
+  // win.show() //手动显示窗口
+
+  //打开调试窗口
+  // win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
-
-
-
